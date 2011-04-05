0819975093677d8d3ff43f59ee7c902859f31e41
Guido Manfredi <gmanfred@pr2c1.(none)>
2011-04-05 12:10:49 +0200

    Add convertion fonctions between Qstr and doubles

--- codels/pr2SoftMotionMainCodels.cc
+++ codels/pr2SoftMotionMainCodels.cc
@@ -481,6 +481,57 @@ pr2SoftMotionGotoQEnd(PR2SM_QSTR *qGoto, int *report)
   return ETHER;
 }
 
+void doubles2QStr(double* src, PR2SM_QSTR* dst)
+{
+  dst->torso= src[0];
+  dst->head_pan= src[1];
+  dst->head_tilt= src[2];
+  dst->laser_tilt= src[3];
+  dst->r_shoulder_pan= src[4];
+  dst->r_shoulder_lift= src[5];
+  dst->r_upper_arm_roll= src[6];
+  dst->r_elbow_flex= src[7];
+  dst->r_forearm_roll= src[8];
+  dst->r_wrist_flex= src[9];
+  dst->r_writ_roll= src[10];
+  dst->r_gripper= src[11];
+  dst->r_gripper_false= src[12];
+  dst->l_shoulder_pan= src[13];
+  dst->l_shoulder_lift= src[14];
+  dst->l_upper_arm_roll= src[15];
+  dst->l_elbow_flex= src[16];
+  dst->l_forearm_roll= src[17];
+  dst->l_wrist_flex= src[18];
+  dst->l_wrist_roll= src[19];
+  dst->l_gripper= src[20];
+  dst->l_gripper_false= src[21];
+}
+
+void QStr2doubles(PR2SM_QSTR* src, double* dst)
+{
+  dst[0]= src->torso;
+  dst[1]= src->head_pan;
+  dst[2]= src->head_tilt;
+  dst[3]= src->laser_tilt;
+  dst[4]= src->r_shoulder_pan;
+  dst[5]= src->r_shoulder_lift;
+  dst[6]= src->r_upper_arm_roll;
+  dst[7]= src->r_elbow_flex;
+  dst[8]= src->r_forearm_roll;
+  dst[9]= src->r_wrist_flex;
+  dst[10]= src->r_writ_roll;
+  dst[11]= src->r_gripper;
+  dst[12]= src->r_gripper_false;
+  dst[13]= src->l_shoulder_pan;
+  dst[14]= src->l_shoulder_lift;
+  dst[15]= src->l_upper_arm_roll;
+  dst[16]= src->l_elbow_flex;
+  dst[17]= src->l_forearm_roll;
+  dst[18]= src->l_wrist_flex;
+  dst[19]= src->l_wrist_roll;
+  dst[20]= src->l_gripper;
+  dst[21]= src->l_gripper_false;
+}
 
 
 
